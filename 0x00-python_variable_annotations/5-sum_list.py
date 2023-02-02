d636666c0d338ee8674bd3c08bf22ae0cc3b73a2
“victornnamdii” <victorilodiuba@gmail.com>
2023-02-02 22:23:04 +0100

    🩹 Fix the import issue

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -2,9 +2,10 @@
 """
 Sum list
 """
+from typing import List
 
 
-def sum_list(input_list: list[float]) -> float:
+def sum_list(input_list: List[float]) -> float:
     """
     Takes a list input_list of floats as argument and
     returns their sum as a float
