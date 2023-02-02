ac58314c3ce1c2d5943ea2a6e00d925cc9de5460
“victornnamdii” <victorilodiuba@gmail.com>
2023-02-02 21:57:01 +0100

    Add: 0x00/3

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+To str func
+"""
+
+
+def to_str(n: float) -> str:
+    """
+    Takes a float n as argument and returns the string
+    representation of the float
+    """
+    return str(n)
