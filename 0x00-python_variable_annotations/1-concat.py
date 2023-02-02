da0d61f816309d6fdc34136d2ae69434cb7e4f1a
“victornnamdii” <victorilodiuba@gmail.com>
2023-02-02 21:33:31 +0100

    Add: 0x00/1

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Concat
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    Takes a string str1 and a string str2 as arguments and returns a
+    concatenated string
+    """
+    return str1 + str2
