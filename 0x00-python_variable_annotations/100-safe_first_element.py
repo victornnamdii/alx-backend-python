dbd4d7c960165ecdde7bdeb69dda3bda285a54c4
Ilodiuba Victor <101352050+victornnamdii@users.noreply.github.com>
2023-02-02 23:34:27 +0100

    Update 100-safe_first_element.py

--- 0x00-python_variable_annotations/100-safe_first_element.py
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -6,6 +6,9 @@ from typing import Sequence, Any, Union
 
 
 def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
+    """
+    Returns first element
+    """
     if lst:
         return lst[0]
     else:
