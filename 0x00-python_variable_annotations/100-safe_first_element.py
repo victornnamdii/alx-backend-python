5d1459496f7f97f18599b20aa82c894a85a0116a
“victornnamdii” <victorilodiuba@gmail.com>
2023-02-02 23:30:20 +0100

    Add: 0x00/100

---
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+safe first element
+"""
+from typing import Sequence, Any, Union
+
+
+def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
+    if lst:
+        return lst[0]
+    else:
+        return None
