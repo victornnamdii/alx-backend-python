7f5bf16c5d42b3461a624ddf339e3a04a6ac3a13
“victornnamdii” <victorilodiuba@gmail.com>
2023-02-27 19:55:55 +0100

    Task 5

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -4,7 +4,7 @@ Learning Unittests and Integration Tests
 """
 
 import unittest
-from unittest.mock import patch, Mock
+from unittest.mock import patch, Mock, PropertyMock
 from client import GithubOrgClient
 from parameterized import parameterized
 from typing import Dict, Callable
@@ -15,8 +15,10 @@ class TestGithubOrgClient(unittest.TestCase):
     Testing GitHubOrgClient
     """
     @parameterized.expand([
-        ("google", {"login": "google", "id": 1342004}),
-        ("abc", {"login": "abc", "id": 1342555})
+        ("google", {"login": "google", "id": 1342004,
+                    "repos_url": "https://api.github.com/orgs/google/repos"}),
+        ("abc", {"login": "abc", "id": 1342555,
+                 "repos_url": "https://api.github.com/orgs/abc/repos"})
     ])
     @patch(
         "client.get_json",
@@ -33,3 +35,16 @@ class TestGithubOrgClient(unittest.TestCase):
         organisation = GithubOrgClient(org)
         self.assertEqual(organisation.org, response)
         mocked_fn.assert_called_once()
+
+    def test_public_repos_url(self):
+        """
+        Testing GithubOrgClient._public_repos_url
+        """
+        with patch("client.GithubOrgClient.org",
+                   new_callable=PropertyMock) as mock_property:
+            mock_property.return_value = {
+                "login": "google", "id": 1342004,
+                "repos_url": "https://api.github.com/orgs/google/repos"
+            }
+            self.assertEqual(GithubOrgClient("google")._public_repos_url,
+                             "https://api.github.com/orgs/google/repos")
